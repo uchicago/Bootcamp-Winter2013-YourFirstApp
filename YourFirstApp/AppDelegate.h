e0f58e503a647b40f743954db50c84b3d97eb5cf
T. Andrew Binkowski <abinkowski@uchicago.edu>
2013-03-08 20:03:08 -0600

    Initial Commit

---
+++ YourFirstApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  YourFirstApp
+//
+//  Created by T. Andrew Binkowski on 3/8/13.
+//  Copyright (c) 2013 T. Andrew Binkowski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
